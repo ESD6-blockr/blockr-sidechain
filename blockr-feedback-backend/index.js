9f3382430557880a094cc1cb035e9e50c880bf90
Kuiper,Mike M <mike.kuiper@student.fontys.nl>
2019-04-09 18:35:54 +0200

    Initial commit

--- blockr-feedback-backend/index.js
+++ blockr-feedback-backend/index.js
@@ -1,1 +1,18 @@
-console.log('I have started!');
+const express = require('express');
+const app = express();
+let bodyParser = require('body-parser');
+
+const port = 3002;
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => res.send('Welcome to back-end of Blockr feedback system!'));
+
+app.post('/login', function (req, res) {
+    console.log(req.body);
+    res.send('login')
+});
+  
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
