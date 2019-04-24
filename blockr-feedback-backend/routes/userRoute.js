89582e0e4be1d27a821ad9a425bfbfbe91b2af1d
Kuiper,Mike M <mike.kuiper@student.fontys.nl>
2019-04-24 11:14:35 +0200

    Implemented functionality for checking match pub/priv keypair

--- blockr-feedback-backend/routes/userRoute.js
+++ blockr-feedback-backend/routes/userRoute.js
@@ -1,73 +1,43 @@
-const naclFactory = require('js-nacl');
 const keys = require('../keyStorage');
 const bip39 = require('bip39');
+const Security = require('../security/security');
+var colors = require('colors');
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-/* @param pubkey 
-* Storing the pubkey in sessionStorage for user convenience*/
-app.post('/login', async function (req, res) {
-    console.log(req.body);
-    res.send('login') // blockchain api call (bestaat de public key)
+  /* @param pubkey 
+  * Storing the pubkey in sessionStorage for user convenience */
+  app.post('/login', async function (req, res) {
+    const pubKey = req.body.pubKey;
+    const privKey = req.body.privKey;
 
     // STEP 1: perform api call to blockchain to check existence of public key
-    // STEP 2: verify that private key is derived from entered public key
+    // Because this function doesn't exist yet on the block we mock it by hardcoding some keys in a map
+    if (!keys.get(pubKey)) {
+      res.status(401);
+      res.send('[Unauthorized] Supplied public key is not present in blockr!');
+    }
+
+    // STEP 2: verify that private key matches with entered public key
+    const message = "blockr blockchain is the best ever made!";
+    const signature = await Security.sign(message, privKey);
+
+    // check if decodedMessage is equal to original message (this means privKey en publicKey are related to each other)
+    let decodedMessage = await Security.verify(signature, pubKey);
+    console.log(`decodedMessage: ${decodedMessage}`.green);
+
+    if(decodedMessage !== message) {
+      res.status(401);
+      res.send('[Unauthorized] Supplied public/private key pair doesn\'t match!');
+    }
+
     // STEP 3: generate token
+    res.send('[Authenticated] Login successful!'); 
 
-    let signature = req.body.signature;
-    let pubKey = req.body.pubKey;
+    // const mnemonic = bip39.entropyToMnemonic('128c73c5050bb6cce9ae2fc9bf1ef1e9');
+    // console.log(mnemonic);
+  });
 
-    if(keys.get(req.body.pubKey)) console.log('Key present');
-    else console.log('key not present');
-
-    const mnemonic = bip39.entropyToMnemonic('128c73c5050bb6cce9ae2fc9bf1ef1e9');
-    console.log(mnemonic);
-
-    // const hexString = nacl.to_hex(nacl.crypto_sign_open(signatureBin, publicKeyBytes)).toString();
-    // console.log(derivedPrivKey)
-
-    let result = await verifyAsync(signature, pubKey);
-    console.log(result)
-});
-
-    //other routes..
-
-  
+  //other routes..
 }
-
-function verifyAsync(signature, pubKey) {
-    return new Promise((resolve, reject) => {
-      if (!signature || !pubKey) {
-        reject(new Error('invalid message'));
-      } else {
-        naclFactory.instantiate((nacl) => {
-          const publicKeyBytes = hexToBytes(pubKey);
-          const signatureBytes = hexToBytes(signature);
-          // Decode message from packet with public key
-          const a = nacl.crypto_sign_open(signatureBytes, publicKeyBytes);
-          console.log('AAAAAAAAAAAAAAAAAAa');
-          console.log(a)
-          const b = nacl.to_hex(a);
-          // Convert hex to string
-          let message = '';
-          for (let i = 0; i < b.length; i += 2) {
-            message += String.fromCharCode(parseInt(b.substr(i, 2), 16));
-          }
-          this.debug(`Message: ${message}`);
-          resolve(message);
-        });
-      }
-    });
-  };
-
- function hexToBytes(hex) {
-    if (!hex) {
-      return new Uint8Array(); // eslint-disable-line no-undef
-    }
-    const bytes = [];
-    for (let i = 0, len = hex.length; i < len; i += 2) {
-      bytes.push(parseInt(hex.substr(i, 2), 16));
-    }
-    return new Uint8Array(bytes); // eslint-disable-line no-undef
-  };
 
