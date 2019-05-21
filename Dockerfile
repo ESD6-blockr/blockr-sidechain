4cb6db8f10e376fa566e1ea42966470cdf7a2ef8
Arnoud Bevers <a.bevers@jasbit.nl>
2019-05-21 18:27:13 +0200

    [TEMP] Started Dockerfile for private IPFS node

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,12 @@
-FROM ubuntu:16.04
-WORKDIR /ipfs
-RUN sudo apt-get update
-RUN sudo apt-install -y jq
-RUN curl -O https://storage.googleapis.com/golang/go1.23.4.linux-amd64.tar.gz
-RUN sudo tar -C /usr/local -xzf go1.23.4.linux-amd64.tar.gz
-RUN export PATH=$PATH:/usr/local/go/bin
-RUN source $HOME/.profile
-RUN sudo apt-get update
-RUN wget https://dist.ipfs.io/go-ipfs/v0.4.19/go-ipfs_v0.4.19_linux-amd64.tar.gz
-RUN tar xvfz go-ipfs_v0.4.18_linux-amd64.tar.gz
-RUN sudo mv go-ipfs/ipfs /usr/local/bin/ipfs
-RUN rm go-ipfs_v0.4.19_linux-amd64.tar.gz
-RUN rm -R ./go-ipfs
-RUN ipfs init
-# RUN sudo apt-get install git
-# RUN go get -u github.com/Kubuxu/go-ipfs-swarm-key-gen/ipfs-swarm-key-gen
-# RUN ./go/bin/ipfs-swarm-key-gen > ~/.ipfs/swarm.key
-# Pregenerated swarm key?
-COPY ./.ipfs ~/.ipfs 
-RUN ipfs bootstrap rm --all
-# IP address of bootnode
-# PeerID of bootnode
-RUN ipfs config show > config.json
-ENV PeerID = $(jq -r '.Identity.PeerID' config.json)
-RUN echo ${PeerID}
+    
+FROM ipfs/go-ipfs:latest
+RUN echo $(ls /data)
+COPY ./.ipfs/swarm.key /data/ipfs/swarm.key
+RUN ls /data/ipfs
+RUN cat /data/ipfs/swarm.key
+# COPY init.sh /usr/local/bin/start_ipfs
+# # RUN chown ipfs:users /usr/local/bin/start_ipfs
+# RUN echo $(whoami)
+# # RUN chown ipfs /usr/local/bin/start_ipfs
+# # RUN chmod +x /usr/local/bin/start_ipfs
+# ENTRYPOINT ["/sbin/tini", "--", "/usr/local/bin/start_ipfs"]
--- blockr-feedback-backend/api/controllers/paper.js
+++ blockr-feedback-backend/api/controllers/paper.js
@@ -10,23 +10,18 @@ Buffer = require("ipfs-http-client").Buffer;
 const sleep = milliseconds => {
   return new Promise(resolve => setTimeout(resolve, milliseconds));
 };
-const ipfs = ipfsClient('/ip4/192.168.25.18/tcp/8080');
+// const ipfs = ipfsClient('/ip4/127.0.0.1/tcp/5001');
+
+const ipfs = ipfsClient('127.0.0.1', '5001', { protocol: 'http' });
 
 // TODO: Check metadata of file
 // TODO: If not textfile (not exists or image), save file to location and tell user where to find the file
 app.get("/:hash", (req, res) => {
   let ipfsHash = req.params.hash;
-  console.log(ipfsHash);
   ipfs.get(`/ipfs/${ipfsHash}`, (error, file) => {
-    console.log(file);
     fs.writeFile(`${__dirname}/test.pdf`, file[0].content, err => {
       sleep(500).then(() => {
         let filePath = path.join(__dirname, "test.pdf");
-        //   // let stat = fs.statSync(filePath);
-        //   // response.setHeader("Content-Length", stat.size);
-        //   // response.setHeader("Content-Type", "application/pdf");
-        //   // response.setHeader("Content-Disposition", "attachment; filename=quote.pdf");
-        //   console.log(filePath);
         let data = fs.readFileSync(filePath);
         res.contentType("application/pdf");
         res.send(data);
@@ -40,22 +35,10 @@ app.get("/:hash", (req, res) => {
  * POST - adds a paper (file) to the IPFS network.
  */
 app.post("/", (request, response) => {
-  // console.log(request.body);
-  // const arrayBuffer = base64_arraybuffer.decode(request.body.file);
-  // console.log(arrayBuffer);
-  // const s = new Readable();
-  // s._read = () => {};
   const base64 = request.body.file.toString();
   const base64Trimmed = base64.split(";base64,").pop();
-  // s.push(base64Trimmed);
-  // s.push(null)
   fs.writeFile("test.pdf", base64Trimmed, { encoding: "base64" }, err => {
-    // ipfs.addFromFs('test.pdf', (err, result) => {
-    //   if(err) { throw err }
-    //   console.log(result);
-    // })
   });
-  // // var buffer = new Buffer(base64Trimmed, 'base64');
   const buffer = Buffer.from(base64Trimmed, "base64");
   ipfs.add(buffer, (err, res) => {
     if (err) throw err;
