5772e8c117ec162d2e9ec718eadf8bfd35fcb0b1
Arnoud Bevers <arnoud@arnoudbevers.com>
2019-06-05 11:30:12 +0200

    [ADD] POST call returns hash, Dockerfile updates

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,6 @@
     
 FROM ipfs/go-ipfs:latest
-RUN echo $(ls /data)
 COPY ./.ipfs/swarm.key /data/ipfs/swarm.key
-RUN ls /data/ipfs
-RUN cat /data/ipfs/swarm.key
-# COPY init.sh /usr/local/bin/start_ipfs
-# # RUN chown ipfs:users /usr/local/bin/start_ipfs
-# RUN echo $(whoami)
-# # RUN chown ipfs /usr/local/bin/start_ipfs
-# # RUN chmod +x /usr/local/bin/start_ipfs
-# ENTRYPOINT ["/sbin/tini", "--", "/usr/local/bin/start_ipfs"]
+COPY init.sh /usr/local/bin/start_ipfs
+RUN chown ipfs:users /usr/local/bin/start_ipfs && chmod +x /usr/local/bin/start_ipfs
+
--- blockr-feedback-backend/api/controllers/paper.js
+++ blockr-feedback-backend/api/controllers/paper.js
@@ -2,29 +2,27 @@ const express = require("express"),
   app = express(),
   path = require("path"),
   ipfsClient = require("ipfs-http-client"),
-  base64_arraybuffer = require("base64-arraybuffer"),
   fs = require("fs"),
-  Readable = require("stream").Readable;
-Buffer = require("ipfs-http-client").Buffer;
+  Buffer = require("ipfs-http-client").Buffer;
 
 const sleep = milliseconds => {
   return new Promise(resolve => setTimeout(resolve, milliseconds));
 };
 
-const ipfs = ipfsClient('blockr.verux.nl', '3031', { protocol: 'http' }); // TODO: Get random file
+const ipfs = ipfsClient("blockr.verux.nl", "3031", { protocol: "http" });
 
-// TODO: Check metadata of file
-// TODO: If not textfile (not exists or image), save file to location and tell user where to find the file
+/**
+ * Gets a file by its IPFS hash
+ */
 app.get("/:hash", (req, res) => {
   let ipfsHash = req.params.hash;
   ipfs.get(`/ipfs/${ipfsHash}`, (error, file) => {
-    console.log(file);
     fs.writeFile(`${__dirname}/test.pdf`, file[0].content, err => {
       sleep(500).then(() => {
         let filePath = path.join(__dirname, "test.pdf");
         let data = fs.readFileSync(filePath);
         res.contentType("application/pdf");
-        res.send(data);
+        res.send(data); //Sends file directly to browser, can be changed to more friendly way
         fs.unlinkSync(filePath);
       });
     });
@@ -33,16 +31,22 @@ app.get("/:hash", (req, res) => {
 
 /**
  * POST - adds a paper (file) to the IPFS network.
+ * Request body requires a base64 encoded string of the file.
+ * Returns following JSON object: 
+ *  {
+ *    hash: "QmU2qh9BRHufNnQTXoyuEaWjipHzGGJaUbJz9s5kvs5Kt9"
+ *  }
  */
 app.post("/", (request, response) => {
-  const base64 = request.body.file.toString();
-  const base64Trimmed = base64.split(";base64,").pop();
-  fs.writeFile("test.pdf", base64Trimmed, { encoding: "base64" }, err => {
-  });
+  const base64 = request.body.base64EncodedPDF.toString();
+  const base64Trimmed = base64.split("data:application/pdf;base64,").pop();
   const buffer = Buffer.from(base64Trimmed, "base64");
   ipfs.add(buffer, (err, res) => {
     if (err) throw err;
     console.log(res);
+    response.send({
+      hash: res[0].hash // [0] since the request returns an array (if you try to upload a folder it returns multiple )
+    });
   });
 });
 
